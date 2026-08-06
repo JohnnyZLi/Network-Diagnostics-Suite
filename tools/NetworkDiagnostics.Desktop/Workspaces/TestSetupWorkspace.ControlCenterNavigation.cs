6d668eb55801e73d3ecaf99a3c205d163e7f7080
Johnny <92900710+JohnnyZLi@users.noreply.github.com>
2026-08-06 14:57:08 -0700

    Open guided inline comparison flow

--- tools/NetworkDiagnostics.Desktop/Workspaces/TestSetupWorkspace.ControlCenterNavigation.cs
+++ tools/NetworkDiagnostics.Desktop/Workspaces/TestSetupWorkspace.ControlCenterNavigation.cs
@@ -1,20 +1,6 @@
-using Avalonia.Threading;
-
 namespace NetworkDiagnostics.Desktop.Workspaces;
 
 public sealed partial class TestSetupWorkspace
 {
-    public void OpenInlineComparison()
-    {
-        compareModeExpanded = true;
-        if (!EnsureControlCenterSections())
-        {
-            Dispatcher.UIThread.Post(OpenInlineComparison);
-            return;
-        }
-
-        RenderRecentDiagnostics();
-        RenderInlineComparison();
-        Dispatcher.UIThread.Post(() => inlineComparisonSection?.BringIntoView());
-    }
+    public void OpenInlineComparison() => OpenProductComparison();
 }
