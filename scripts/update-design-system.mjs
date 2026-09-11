1fae008adbff0dcbbc75fc285572161aa2326836
Johnny <92900710+JohnnyZLi@users.noreply.github.com>
2026-09-10 17:37:24 -0700

    Remove stale local design-system update wrapper

--- scripts/update-design-system.mjs
+++
@@ -1,4 +0,0 @@
-import { resolveConsumerRelease } from "./design-system-consumer-release.mjs";
-
-const release = await resolveConsumerRelease();
-console.log(`Locked ${release.package} v${release.version} at ${release.sourceCommit}.`);
